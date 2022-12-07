3cc5e0545c32dab917d66e7a9993d01df4dd10b6
zacke <zackepe@gmail.com>
2022-12-07 19:46:46 +0200

    tehtävä 9.2

---
+++ week1/exercise9.2.py
@@ -0,0 +1,28 @@
+class Car:
+
+    def __init__(self, licensenum, topspeed, currentspeed=0, distancetraveled=0):
+        self.licensenum = licensenum
+        self.topspeed = topspeed
+        self.currentspeed = currentspeed
+        self.distancetraveled = distancetraveled
+
+    def accelerate(self, speedchange):
+        if self.currentspeed + speedchange < self.topspeed:
+            if self.currentspeed + speedchange > 0:
+                self.currentspeed = self.currentspeed + speedchange
+            else:
+                self.currentspeed = 0
+        else:
+            self.currentspeed = self.topspeed
+
+
+newcar = Car("ABC-123", 142)
+print(f"Auton rekisteritunnus: {newcar.licensenum}\nHuippunopeus: {newcar.topspeed} km/h")
+print(f"Tämänhetkinen nopeus: {newcar.currentspeed} km/h\nAjettu matka: {newcar.distancetraveled} km\n")
+
+newcar.accelerate(30)
+newcar.accelerate(70)
+newcar.accelerate(50)
+print(f"Auton nopeus on {newcar.currentspeed} km/h.")
+newcar.accelerate(-200)
+print(f"Auton nopeus hätäjarrutuksen jälkeen on {newcar.currentspeed} km/h.")
